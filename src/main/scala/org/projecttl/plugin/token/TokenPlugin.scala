f7baf87736b5c5edf57b06f38dd4db78cefe22be
Project_TL <kimpiao020@gmail.com>
2021-01-29 17:05:43 +0900

    Add: Add event InteractionAnimalToken.scala with TokenPlugin

--- src/main/scala/org/projecttl/plugin/token/TokenPlugin.scala
+++ src/main/scala/org/projecttl/plugin/token/TokenPlugin.scala
@@ -4,14 +4,19 @@ import org.bukkit.ChatColor
 import org.bukkit.plugin.java.JavaPlugin
 import org.projecttl.plugin.token.command.GetToken
 import org.projecttl.plugin.token.command.arguments.ArgumentForGetToken
+import org.projecttl.plugin.token.listeners.InteractionAnimalToken
 
 class TokenPlugin extends JavaPlugin {
+
+  private val serverManager = getServer.getPluginManager
 
   override def onEnable(): Unit = {
     getLogger.info(s"${ChatColor.YELLOW}The plugin has enable.")
 
     getCommand("token").setExecutor(new GetToken)
     getCommand("token").setTabCompleter(new ArgumentForGetToken)
+
+    serverManager.registerEvents(new InteractionAnimalToken, this)
   }
 
   override def onDisable(): Unit = {
